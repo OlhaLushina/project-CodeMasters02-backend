d4050443e59399e42f0066ba9b311ed5e8948cfc
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-10 18:29:23 +0300

    Create review.js

---
+++ models/review.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
+
+// Схема mongoose
+const reviewSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      required: [true, "Rating is required"],
+    },
+    text: {
+      type: String,
+      required: [true, "Review text is required"],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError); // якщо валідація не пройде, то видасть помилку
+
+//Схема валідації Joi
+const sendReviewSchema = Joi.object({
+  rating: Joi.number().required(), //
+  text: Joi.string().max(300).required(),
+});
+
+// Створюємо модель
+const Review = model("review", reviewSchema);
+
+module.exports = {
+  Review,
+  sendReviewSchema,
+};
