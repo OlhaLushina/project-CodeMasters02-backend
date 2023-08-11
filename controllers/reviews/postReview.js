9ce84c645f4eeaa7785cabaa8aaa9825e4689a28
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-11 22:52:25 +0300

    Update postReview.js

--- controllers/reviews/postReview.js
+++ controllers/reviews/postReview.js
@@ -1,12 +1,12 @@
+const { HttpError } = require("../../helpers");
 const { Review } = require("../../models/review");
 
 const add = async (req, res) => {
   const { _id: owner } = req.user;
-  console.log(req.user);
 
   const review = await Review.create({ ...req.body, owner });
 
-  res.status(201).json({ review });
+  res.status(201).json(review);
 };
 
 module.exports = add;
