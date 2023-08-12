8ebf338b2787c28171258ea1e6946b1eb050e573
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-13 01:09:20 +0300

    Create addTask.js

---
+++ controllers/tasks/addTask.js
@@ -0,0 +1,17 @@
+const { isLaterTime, HttpError } = require("../../helpers");
+const { Task } = require("../../models/task");
+
+const addTask = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { start, end } = req.body;
+
+  if (!isLaterTime(start, end)) {
+    throw HttpError(400, "Start time can't be in later then end time");
+  }
+
+  const newTask = await Task.create({ ...req.body, owner });
+
+  res.status(201).json(newTask);
+};
+
+module.exports = addTask;
