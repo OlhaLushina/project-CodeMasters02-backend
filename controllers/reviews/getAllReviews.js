0e92f08402b34249671d77890d16055e9be77c68
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-13 14:44:00 +0300

    Update getAllReviews.js

--- controllers/reviews/getAllReviews.js
+++ controllers/reviews/getAllReviews.js
@@ -1,16 +1,18 @@
-const { HttpError } = require("../../helpers");
 const { Review } = require("../../models/review");
+const { ctrlWrapper } = require("../../helpers");
 
 //Повертає всі відгуки
 
-const getAll = async (req, res) => {
+const getAllReviews = async (req, res) => {
   const allReviews = await Review.find()
     .sort({
       createdAt: -1,
     })
-    .populate("owner", "name avatar"); //avatar
+    .populate("owner", "name avatar");
 
   res.json(allReviews);
 };
 
-module.exports = getAll;
+module.exports = {
+  getAllReviews: ctrlWrapper(getAllReviews),
+};
