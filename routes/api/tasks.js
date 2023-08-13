d623dfeec2bc1e95409d06e94a6a96c75d8c65f3
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-13 14:44:26 +0300

    Create tasks.js

---
+++ routes/api/tasks.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { validateBody, authenticate } = require("../../middlewares");
+const { addTaskSchema, editTaskSchema } = require("../../models/task");
+const ctrl = require("../../controllers/tasks");
+
+const router = express.Router();
+
+// Повертає всі завдання за місяць
+router.get("/", authenticate, ctrl.getAllTasksOfMonth);
+
+// Додати завдання
+router.post("/", authenticate, validateBody(addTaskSchema), ctrl.addTask);
+
+router.patch("/:id", authenticate, validateBody(editTaskSchema), ctrl.editTask);
+
+router.delete("/:id", authenticate, ctrl.deleteTask);
+
+module.exports = router;
