8a6a7beca566290c85d321476261ac9e47651335
Olha Lushina <olga.lushina.ua@gmail.com>
2023-08-19 02:02:37 +0300

    test

--- controllers/tasks/getAllTasksOfMonth.js
+++ controllers/tasks/getAllTasksOfMonth.js
@@ -1,10 +1,13 @@
 const { Task } = require("../../models/task");
 
 const getAllTasksOfMonth = async (req, res) => {
+  /*
   const { _id: owner } = req.user;
   const { dateFrom, dateTo } = req.query;
 
   const tasksList = await Task.find({ owner, date: { $gte: dateFrom, $lte: dateTo } });
+  */
+   const tasksList = await Task.find({date: { $gte: dateFrom, $lte: dateTo } });
 
   res.json(tasksList);
 };
