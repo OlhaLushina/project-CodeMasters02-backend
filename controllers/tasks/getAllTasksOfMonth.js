7f327a2e39b0465e82b329b07c07ae9c142bdc49
Olha Lushina <olga.lushina.ua@gmail.com>
2023-08-19 02:04:31 +0300

    Update getAllTasksOfMonth.js

--- controllers/tasks/getAllTasksOfMonth.js
+++ controllers/tasks/getAllTasksOfMonth.js
@@ -7,6 +7,8 @@ const getAllTasksOfMonth = async (req, res) => {
 
   const tasksList = await Task.find({ owner, date: { $gte: dateFrom, $lte: dateTo } });
   */
+  
+   const { dateFrom, dateTo } = req.query;
    const tasksList = await Task.find({date: { $gte: dateFrom, $lte: dateTo } });
 
   res.json(tasksList);
