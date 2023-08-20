6bcbd1c346c41e42478e8110cfccc26c65078e2e
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-08-20 21:36:13 +0300

    Update getAllTasksOfMonth.js

--- controllers/tasks/getAllTasksOfMonth.js
+++ controllers/tasks/getAllTasksOfMonth.js
@@ -5,7 +5,13 @@ const getAllTasksOfMonth = async (req, res) => {
   const { _id: owner } = req.user;
   const { dateFrom, dateTo } = req.query;
 
-  const tasksList = await Task.find({ owner, date: { $gte: dateFrom, $lte: dateTo } });
+  const startDate = new Date(dateFrom);
+  startDate.setHours(0, 0, 0, 0);
+
+  const endDate = new Date(dateTo);
+  endDate.setHours(23, 59, 59, 999);
+
+  const tasksList = await Task.find({ owner, date: { $gte: startDate, $lte: endDate } });
 
   res.json(tasksList);
 };
