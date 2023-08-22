1f4a3d141ce38e45b85e4439b387b06ffcf0b58c
Olha Lushina <olga.lushina.ua@gmail.com>
2023-08-22 14:29:23 +0300

    Update getOwnReview.js

--- controllers/reviews/getOwnReview.js
+++ controllers/reviews/getOwnReview.js
@@ -7,14 +7,17 @@ const getOwnReview = async (req, res) => {
   // Шукаємо відгук даного користувача
   const ownReview = await Review.findOne({ owner });
 
+  /*
   if (!ownReview) {
     throw HttpError(404, "Not found");
   }
-
-  res.json({
+  */
+  const answer = (ownReview) ? {
     rating: ownReview.rating,
     text: ownReview.text,
-  });
+  } : {};
+
+  res.json(answer);
 };
 
 module.exports = {
