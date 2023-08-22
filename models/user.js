6ae9bdf3212028a91d4814cb95609332cdad73fb
Olha Lushina <olga.lushina.ua@gmail.com>
2023-08-22 14:08:56 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const Joi = require('joi');
 
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // регуляйрний вираз для email
 
-const phoneRegexp = /^\+38 \(\d{3}\) \d{3}-\d{4}$/; // регуляйрний вираз для телефона
+//const phoneRegexp = /^\+38 \(\d{3}\) \d{3}-\d{4}$/; // регуляйрний вираз для телефона
 
-const birthdayRegexp =  /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/((19|20)\d\d)$/; // регуляйрний вираз для дати народження
+//const birthdayRegexp =  /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/((19|20)\d\d)$/; // регуляйрний вираз для дати народження
+const birthdayRegexp =  /^\d{4}-\d{2}-\d{2}$/; // регуляйрний вираз для дати народження
+
 
 // Схема валідації mongoose (даних, які хочемо зберегти)
 const userSchema = new Schema({
@@ -32,12 +34,12 @@ const userSchema = new Schema({
   birthday: {
     type: String,
     maxlength: 10,
+    match: birthdayRegexp,
     default: "",
   },
   phone: {
     type: String,
     maxlength: 18,
-    match: phoneRegexp,
     default: "",
   },
   skype: {
@@ -79,7 +81,7 @@ const editSchema = Joi.object({
     name: Joi.string().max(16).required(),
     email: Joi.string().max(254).pattern(emailRegexp).required(),
     birthday: Joi.string().max(10).pattern(birthdayRegexp).allow(""),
-    phone: Joi.string().max(18).pattern(phoneRegexp).allow(""),
+    phone: Joi.string().max(18).allow(""),
     skype: Joi.string().max(254).allow(""),
     avatar: Joi.string().max(254).allow(""),
 });
